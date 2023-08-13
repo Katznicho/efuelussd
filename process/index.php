2fb4698ef700c6f8c10ea05659fa1079971a10fd
katznicho <katznicho@gmail.com>
2023-08-13 18:25:05 +0300

    end-no-edit

--- process/index.php
+++ process/index.php
@@ -3,33 +3,33 @@
  
 
 //include("process.php");
-include_once 'fuelsystem/checker.php';
-$checker=new checker();
+// include_once 'fuelsystem/checker.php';
+// $checker=new checker();
 
-if($checker->checkbodaexist()==TRUE)
-{
-   // echo "welcome Boda";
-    include("fuelsystem/Boda.php");
+// if($checker->checkbodaexist()==TRUE)
+// {
+//    // echo "welcome Boda";
+//     include("fuelsystem/Boda.php");
     
-}
-elseif($checker->checkAgentexist()==TRUE)
-{
-    include("fuelsystem/Agent.php"); 
-}
-else
-{
+// }
+// elseif($checker->checkAgentexist()==TRUE)
+// {
+//     include("fuelsystem/Agent.php"); 
+// }
+// else
+// {
     
-    //echo "hello";
-    include("process.php"); 
-}
+//     //echo "hello";
+//     include("process.php"); 
+// }
 
-//  function writeResponse($msg,$isend = false){
-// 		$resp_msg = 'responseString='.urlencode($msg);
-// 		 if($isend)
-// 		 	$resp_msg .= '&action=end';
-// 		 else
-// 			$resp_msg .= '&action=request';
-//          echo $resp_msg;
-// 	}
+ function writeResponse($msg,$isend = false){
+		$resp_msg = 'responseString='.urlencode($msg);
+		 if($isend)
+		 	$resp_msg .= '&action=end';
+		 else
+			$resp_msg .= '&action=request';
+         echo $resp_msg;
+	}
        
-//     writeResponse("Welcome Creditplud Boda Loan",true);
+    writeResponse("Welcome Creditplud Boda Loan",true);
