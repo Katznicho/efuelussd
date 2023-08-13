2ecdc5326e845c0dbc338cafefb449e96fa8a70a
katznicho <katznicho@gmail.com>
2023-08-13 18:39:31 +0300

    end-no-edit

--- process/index.php
+++ process/index.php
@@ -18,18 +18,19 @@ function writeResponse($msg, $isend = false) {
 // Your existing logic to determine which section to include
 
 // Uncomment and modify the section you want to include
-// if ($checker->checkbodaexist() == TRUE) {
-//     include("fuelsystem/Boda.php");
-// } elseif ($checker->checkAgentexist() == TRUE) {
-//     include("fuelsystem/Agent.php");
-// } else {
-//     include("process.php");
-// }
+if ($checker->checkbodaexist() == TRUE) {
+    include("fuelsystem/Boda.php");
+} 
+elseif ($checker->checkAgentexist() == TRUE) {
+    include("fuelsystem/Agent.php");
+} 
+else {
+    writeResponse("Your Not Registered on E-Fuel", true);
+}
 
 // Use writeResponse to send the appropriate message
 // For example, sending a continuation message:
 // writeResponse("Please select an option:\n1. Check Balance\n2. Make a Payment");
 
 // Sending an ending message:
-writeResponse("Welcome Creditplud Boda Loan", true);
-?>
+
