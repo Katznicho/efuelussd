48dd22324d3e458be0454e394fafdfaf0291f340
katznicho <katznicho@gmail.com>
2023-08-15 11:17:07 +0300

    setup payment

--- process/fuelsystem/payments.php
+++ process/fuelsystem/payments.php
@@ -6,13 +6,13 @@ class payments
 
     private  $deposit_url = 'https://wallet.ssentezo.com/api/deposit';
     private  $withdraw_url = 'https://wallet.ssentezo.com/api/withdraw';
-    private  $username = "b7f4211a-24aa-4deb-a990-14dcc1f6b514";
-    private  $password = "5ebb70e03f4b47bcfab0d87561ab5b8f";
-    private $sucessRedirectLink = "http://boda.creditplus.ug/public_html/creditpluswebapp/views/payments/finished.php";
+    private  $username = "611b26c4-2c6c-407c-ae24-c86029434b04";
+    private  $password = "0b7062a15fc9a8417d25be8bcfde11ac";
+    private $sucessRedirectLink = "http://207.154.233.21/efuelussd/views/payments/finished.php";
 
 
 
-    public function deposit($amount, $reference, $phone, $reason="paying back a loan")
+    public function deposit($amount, $reference, $phone, $reason = "paying back a loan")
     {
 
         $payload = array(
@@ -20,21 +20,19 @@ class payments
             "amount" => $amount,
             "reason" => $reason,
             "externalReference" => $reference,
-            "callback" =>$this->sucessRedirectLink,
-            "currency" =>"UGX",
-            "environment" =>"production",
+            "callback" => $this->sucessRedirectLink,
+            "currency" => "UGX",
+            "environment" => "production",
         );
         $response = $this->sendRequest($payload, $this->deposit_url);
-    
+
         return $response;
     }
 
-    public function withdraw($amount, $reference, $phone, $reason="with drawing")
+    public function withdraw($amount, $reference, $phone, $reason = "with drawing")
     {
 
-        $payload = array(
-
-        );
+        $payload = array();
         $response = $this->sendRequest($payload, $this->withdraw_url);
         return $response;
     }
@@ -42,28 +40,31 @@ class payments
 
     private  function sendRequest($payload, $endpoint)
     {
+
+
         $curl = curl_init();
 
         curl_setopt_array($curl, array(
-          CURLOPT_URL => $endpoint,
-          CURLOPT_RETURNTRANSFER => true,
-          CURLOPT_ENCODING => '',
-          CURLOPT_MAXREDIRS => 10,
-          CURLOPT_TIMEOUT => 0,
-          CURLOPT_FOLLOWLOCATION => true,
-          CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-          CURLOPT_CUSTOMREQUEST => 'POST',
-          CURLOPT_POSTFIELDS => $payload,
-          CURLOPT_HTTPHEADER => array(
-            'Authorization: Basic YjdmNDIxMWEtMjRhYS00ZGViLWE5OTAtMTRkY2MxZjZiNTE0OjVlYmI3MGUwM2Y0YjQ3YmNmYWIwZDg3NTYxYWI1Yjhm',
-            'Cookie: PHPSESSID=611rckasb6rj6fs70g28cpcl3d'
-          ),
+            CURLOPT_URL => $endpoint,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS =>$payload,
+            CURLOPT_HTTPHEADER => array(
+                'Authorization: Basic NjExYjI2YzQtMmM2Yy00MDdjLWFlMjQtYzg2MDI5NDM0YjA0OjBiNzA2MmExNWZjOWE4NDE3ZDI1YmU4YmNmZGUxMWFj',
+                'Cookie: PHPSESSID=2mmajnr4lh0dkb11lnh5hopba6'
+            ),
         ));
-        
+
         $response = curl_exec($curl);
-    
+
         curl_close($curl);
-      
+    
+
         return $response;
     }
 
